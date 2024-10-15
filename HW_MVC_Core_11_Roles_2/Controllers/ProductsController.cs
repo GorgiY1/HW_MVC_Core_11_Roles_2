dfa3dacf475a748828b227052312e9eb171d8ded
johnny <orgeorgij@gmail.com>
2024-10-15 20:00:45 +0300

    Add project files.

---
+++ HW_MVC_Core_11_Roles_2/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop_app.Models;
+
+namespace Shop_app.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public ViewResult Create() => View();
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Description")] Product product)
+        {
+            return RedirectToAction(nameof(Index));    
+        }
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public ViewResult Update() => View();
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id, [Bind("Id,Name,Price,Description")] Product product)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public ViewResult Delete() => View();
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
